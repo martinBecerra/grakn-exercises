95baa392c5e8b115f28697266933804bdf35ad34
Martin Becerra <martinbecerra@MacBook-Pro-de-Martin.local>
2019-05-18 14:31:01 -0300

    .sh files updated

--- 02 - Books/loadOntology.sh
+++ 02 - Books/loadOntology.sh
@@ -4,4 +4,3 @@ graql=/media/martin/BlackData/Development/Grakn/linux/grakn-core-1.4.3/graql
 
 $graql console -f book-ontology.gql -k books
 $graql console -f book-rules.gql -k books
-$graql console -f book-data.gql  -k books
